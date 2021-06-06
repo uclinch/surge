0a811269cb94b90522c9265c497360931b16c87d
uclinch <619591430@qq.com>
2021-06-07 07:59:02 +0800

    Update Embyswitch.js

--- Embyswitch.js
+++ Embyswitch.js
@@ -4,7 +4,7 @@ var h = d.getHours();
 const EmbyDay   = '免流🚀';
 const EmbyNight = 'Direct';
 
-if (h >= 19 || h <8) {
+if (h >= 19 || h <8.5) {
     var EmbyNewPolicy = EmbyNight;
 } else {
     var EmbyNewPolicy = EmbyDay;
