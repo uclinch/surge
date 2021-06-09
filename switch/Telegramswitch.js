83c62d27a7a51f9c75c1d89ae7ec3c9bb5fefda9
uclinch <619591430@qq.com>
2021-06-09 21:52:13 +0800

    中文化

--- switch/Telegramswitch.js
+++ switch/Telegramswitch.js
@@ -1,17 +1,17 @@
 var d = new Date();
 var h = d.getHours();
 
-const TwitchDay   = 'Proxy';
-const TwitchNight = '影视专线';
+const TelegramDay   = 'Proxy';
+const TelegramNight = '影视专线';
 
 if (h >= 21 || h <1) {
-    var TwitchNewPolicy = TwitchNight;
+    var TelegramNewPolicy = TelegramNight;
 } else {
-    var TwitchNewPolicy = TwitchDay;
+    var TelegramNewPolicy = TelegramDay;
 }
 
-$surge.setSelectGroupPolicy('Telegram', TwitchNewPolicy);
-$notification.post("Script called", "TelegramSwitch.js", "'Telegram' switched to '" + TwitchNewPolicy + "'");
-console.log("TelegramSwitch.js script: 'Telegram' switched to '" + TwitchNewPolicy + "'");
+$surge.setSelectGroupPolicy('Telegram', TelegramNewPolicy);
+$notification.post("定时切换", "嗖的一下~", "'Telegram' 切换到 '" + TelegramNewPolicy + "'");
+console.log("嗖的一下~ script: 'Telegram' 切换到 '" + TelegramNewPolicy + "'");
 
 $done();
